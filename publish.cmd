e117447634339e23be24a77af7f03df35ab4a40b
AmarnathBhamidipaty <amarnth630@gmail.com>
2018-09-19 18:17:20 +0530

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=evolve404-Web-Deploy.pubxml -p:Password=h04y9nprDi5agWFqYw928LA2wXz3besKlsJMTfyxjqa059m9NAmma5y71Bsl
+
